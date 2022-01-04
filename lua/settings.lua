cf6139dbff275f94d652700a2165eb1ae21c3016
Rajat Rajput <rajputrajat@gmail.com>
2022-01-04 22:34:55 +0530

    autosave vim session

--- lua/settings.lua
+++ lua/settings.lua
@@ -52,3 +52,25 @@ end
 g.ale_sign_column_always = 1
 
 cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
+
+vim.cmd [[
+fu! SaveSess()
+    execute 'mksession! ' . getcwd() . '/.session.vim'
+endfunction
+
+fu! RestoreSess()
+if filereadable(getcwd() . '/.session.vim')
+    execute 'so ' . getcwd() . '/.session.vim'
+    if bufexists(1)
+        for l in range(1, bufnr('$'))
+            if bufwinnr(l) == -1
+                exec 'sbuffer ' . l
+            endif
+        endfor
+    endif
+endif
+endfunction
+
+autocmd VimLeave * call SaveSess()
+autocmd VimEnter * nested call RestoreSess()
+]]
